f52de80ace0792ed168da17c2e81868ecf723d68
Emirhan Eren <emirhanneren52@gmail.com>
2022-12-07 14:44:36 +0300

    7.12.2022

--- OtelProject/Formlar/Grafikler/FrmOdaDolulukGrafigi.cs
+++ OtelProject/Formlar/Grafikler/FrmOdaDolulukGrafigi.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OtelProject.Entity;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,17 @@ namespace OtelProject.Formlar.Grafikler
         public FrmOdaDolulukGrafigi()
         {
             InitializeComponent();
+        }
+        //Database ile Formu bağla
+        DbOtelEntities db = new DbOtelEntities();
+        private void FrmOdaDolulukGrafigi_Load(object sender, EventArgs e)
+        {
+            //Oda Doluluk Grafiği
+            var durumlar = db.OdaDurum();
+            foreach (var x in durumlar)
+            {
+                chartControl1.Series["Odalar"].Points.AddPoint(x.DurumAd, double.Parse(x.Sayı.ToString()));
+            }
         }
     }
 }
